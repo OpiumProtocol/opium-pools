a0099c39b0b38a14ac21ded3a244f17d16a13bf0
Ali Nuraldin <alirunwwc@gmail.com>
2022-07-03 16:28:36 +0200

    Turned on optimizations

--- contracts/interfaces/IStakingModule.sol
+++ contracts/interfaces/IStakingModule.sol
@@ -9,6 +9,22 @@ interface IStakingModule {
   event UnscheduledWithdrawal(address indexed owner, uint256 shares);
   event AssetsClaimed(address indexed owner, uint256 assets);
   event RageQuit(address indexed caller, address indexed receiver, address indexed owner, uint256 shares);
+  event Referral(uint256 indexed id);
 
+  function canDeposit() external view returns (bool);
+  function canWithdraw() external view returns (bool);
+  function getScheduledShares(address receiver) external view returns (uint256 scheduledShares);
+  function getScheduledAssets(address receiver) external view returns (uint256 scheduledAssets);
+
+  function depositRef(uint256 assets, address receiver, uint256 referralId) external returns (uint256 shares);
+  function mintRef(uint256 shares, address receiver, uint256 referralId) external returns (uint256 assets);
+  function scheduleDeposit(uint256 assets, address receiver) external returns (uint256 shares);
+  function scheduleDepositRef(uint256 assets, address receiver, uint256 referralId) external returns (uint256 shares);
+  function unscheduleDeposit(uint256 assets) external;
+  function claimScheduledShares(uint256 shares, bool claimAll) external;
+  function scheduleWithdrawal(uint256 shares, address receiver, address owner) external returns (uint256 assets);
+  function unscheduleWithdrawal(uint256 shares) external;
+  function claimScheduledAssets(uint256 assets, bool claimAll) external;
+  function rageQuit(uint256 shares, address receiver, address owner, address[] calldata tokens) external;
   function postRebalancing() external;
 }
